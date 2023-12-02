37f9455ffe0fb3ca32049978ebab43ebaca004cd
Njordr <njordrenterprises@gmail.com>
2023-12-02 16:45:16 -0600

    Rust commit 1
    Exercises a1 and a2
    
    Additional work on typescript exercise

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,36 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+///----->First try
+// fn main() {
+//     print!({$first_name}, {$last_name});
+// }
+
+// fn first_name() {
+//     let first_name = "Bryce";
+//     println!({$first_name})
+// }
+
+// fn last_name() {
+//     let last_name = "Cram";
+//     print!({$last_name})
+// }
+//
+//
+//
+fn first_name() {
+    println!("Bryce")
+}
+
+fn last_name() {
+    println!("Cram")
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
+
+
+
