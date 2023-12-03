f25dc85d5ab1afd5909baf242cdd2fd6b8be85e8
Njordr <njordrenterprises@gmail.com>
2023-12-02 23:41:07 -0600

    Flow control
    Flow control exercise

--- activities/src/bin/a3a.rs
+++ activities/src/bin/a3a.rs
@@ -10,4 +10,33 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+// * Displays a message based on the value of a boolean variable
+// * When the variable is set to true, display "hello"
+// * When the variable is set to false, display "goodbye"
+
+//first attempt
+// fn main() { 
+//     let a = 1;
+//     if a == 1 {
+//     a = true;
+//         println!("Hello");
+
+//     } else if a !=1 {
+//         a = false; {
+//         println!("Goodbye");
+//         }
+//     }
+// }
+//
+
+ // * Displays a message based on the value of a boolean variable
+fn main() {
+   let a = false;
+ // * When the variable is set to true, display "hello"
+    if a == true {
+        println!("Hello");
+    } else {
+        println!("goodbye");
+    }
+}
