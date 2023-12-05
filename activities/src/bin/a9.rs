b4a59e8aa3af5222296d314891b280502bd729ed
Njordr <njordrenterprises@gmail.com>
2023-12-04 21:05:18 -0600

    tuples exercise
    tuples

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,38 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+//-----> first try 
+// fn main() {   
+// // * Use a function that returns a tuple
+//     fn cart_check(){
+//         let coord = (x, y);
+//         return coord;
+//     }    
+
+// // * Destructure the return value into two variables
+
+//     let (">", "<") = (coord.0, coord.1);
+
+// // * Use an if..else if..else block to determine what to print
+//     if coord.1 > 5 => println!({?}, coord.0);
+// }   
+// }
+// //----->NOT TODAY
+
+
+// * Use a function that returns a tuple
+fn coordinate() -> (i32, i32) {
+    (1, 7)
+}
+fn main() {
+// * Destructure the return value into two variables
+    let (_x, _y) = coordinate();
+// * Use an if..else if..else block to determine what to print
+    if _y > 5 {
+        println!(">5");
+    } else if _y < 5 {
+        println!("<5");
+    } else {
+        println!("=5");
+    }
+}
