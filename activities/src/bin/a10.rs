60b6de6fbdd6c02a663873f134f04c93183a6aa9
Njordr <njordrenterprises@gmail.com>
2023-12-04 22:00:41 -0600

    rust expressions exercise
    expressions

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,4 +12,35 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+//first try 
+// enum var_size {
+//     big,
+//     small
+// };
+
+// fn main() {
+//     let num_var = 5;
+//  let var_accept = check_var_size {
+//         if num_var > 100 -> true;
+//     } else num_var < 100 -> false;
+
+// fn print_size() {
+//     println!(":?", check_var_size)
+// }
+
+// }
+
+//not quite...
+
+fn print_message(gt_100: bool){
+    match gt_100 {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
+fn main() {
+    // boolean to  expression
+    let value = 100;
+    let is_gt_100 = value > 100;//let can be used in place of logical "is"
+    print_message(is_gt_100);
+   }
