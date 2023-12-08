5cef0b6beda00fd1fb46280d8440db70ae49f7bd
Njordr <njordrenterprises@gmail.com>
2023-12-07 18:12:43 -0600

    vectors and for loops
    vectors
    for loops
    borrowing

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,33 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+//first try
+// let some_numbers = vec![10, 20, "thirty", 40];
+
+// for numb in some_numbers {
+//     println!(":?", numb);
+// } else if 
+
+
+// fn main() {}
+
+// // hmmmm
+
+
+fn main() {
+    // Use a vector to store 4 numbers
+    let my_numbers = vec![10, 20, 30, 40];
+    
+    // Iterate through the vector using a for..in loop
+    for num in &my_numbers {
+        // Determine whether to print the number or print "thirty" inside the loop
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{:?}", num), // match can be used to do stuff
+        } 
+    }
+    // Use the .len() function to print the number of elements in a vector
+    println!("number of elements = {:?}", &my_numbers.len()); // print number of elements
+ }
+
+//always to remember to borrow values if using more then once!!!<--------------
