07d7ae971d13e4345141982ca1000dd009c82ec2
Njordr <njordrenterprises@gmail.com>
2023-12-18 21:34:09 -0600

    a18
    a18

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -16,4 +16,59 @@
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
 
-fn main() {}
+//First Try
+
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+//   * Implement Debug print functionality using `derive`
+// #[Derive(Debug)]
+// struct Adult {
+// //   * The structure must contain the person's name and age
+//     name: String,
+//     age: u8, //up to 255 (u8)
+// }
+
+// // * Implement a `new` function for the `Adult` structure that returns a Result:
+// fn check_age(age:) -> Result<(), u8> {
+//     if 
+// }
+//     if
+// }
+// fn main() {
+//     let age = check_age(25);
+
+// }
+// not a chance
+
+#[derive(Debug)]
+struct Adult {
+    age: u8,
+    name: String,
+}
+
+impl Adult {
+    fn new(age: u8, name: &str) -> Result<Self, &str> { //make a new adult
+        if age >= 21 {
+            Ok(Self {
+                age,
+                name: name.to_string()
+            })
+        } else {
+            Err("Age must be at least 21")
+        }                                                //Self is a check if its an adult.
+
+    }
+}
+
+fn main() {
+    let child = Adult::new(15, "Anita");
+    let adult = Adult::new(21, "Sanjay");
+match child {
+        Ok(child) => println!("{} is {} years old", child.name, child.age),
+        Err(e) => println!("{e}"),     
+    }
+match adult {
+        Ok(adult) => println!("{} is {} years old", adult.name, adult.age),
+        Err(e) => println!("{e}"),     
+    }
+    
+}
